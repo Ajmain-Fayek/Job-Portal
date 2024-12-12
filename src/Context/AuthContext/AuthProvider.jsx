32608ad58382a10fce363e918fc3246a1df4a3d0
Ajmain Fayek <ajmainfayek733@gmail.com>
2024-12-12 18:25:31 +0600

    User State Observer Complete

--- src/Context/AuthContext/AuthProvider.jsx
+++ src/Context/AuthContext/AuthProvider.jsx
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AuthContext from "./AuthContext";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+    createUserWithEmailAndPassword,
+    onAuthStateChanged,
+} from "firebase/auth";
 import auth from "../../Firebase/Firebase.config";
 
 const AuthProvider = ({ children }) => {
@@ -11,6 +14,15 @@ const AuthProvider = ({ children }) => {
         setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password);
     };
+
+    useEffect(() => {
+        const clear = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            console.log("Sate captured", currentUser);
+            setLoading(false);
+        });
+        return clear();
+    }, []);
 
     const authInfo = { user, loading, register };
     return (
