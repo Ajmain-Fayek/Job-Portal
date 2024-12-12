14350d3db630060c87f33d6b339fc4c8a44cacb3
Ajmain Fayek <ajmainfayek733@gmail.com>
2024-12-12 20:25:14 +0600

    Footer tempalte added

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router";
 import Navbar from "../Pages/Common/Navbar";
+import Footer from "../Pages/Common/Footer";
 
 const MainLayout = () => {
     return (
         <div>
             <Navbar />
             <Outlet />
+            <Footer />
         </div>
     );
 };
